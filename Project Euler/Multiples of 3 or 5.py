c3548374b78d8718d6540da175350498b90c0be1
Nico4190 <reyesjose429@gmail.com>
2025-09-06 20:58:30 -0600

    Add files via upload

---
+++ Project Euler/Multiples of 3 or 5.py
@@ -0,0 +1,15 @@
+
+
+
+def multiplosde3e5(n):
+
+    suma = 0
+    for i in range(n):
+
+        if i % 3 == 0 or i % 5 == 0:
+            suma += i
+
+    return suma
+
+input_usuario = int(input("Ingrese un numero: "))
+print(multiplosde3e5(input_usuario))
